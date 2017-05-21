9b2718d89acd60ee0bfce90dac1b2887404de7e3
Pedro <pedroovil@gmail.com>
2017-05-21 12:25:43 +0100

    save canvas

--- lobby/public/paint.js
+++ lobby/public/paint.js
@@ -215,6 +215,46 @@ document.getElementById("clear").addEventListener("click", function clear(){
     }
 });
 
+// download canvas
+var callDownload = function() {
+        download(sketch,'apontamento.png');
+        };
+    
+    document.getElementById("save").addEventListener("click", callDownload);
+    
+    function download(canvas, filename) {
+
+    //create a dummy CANVAS
+
+    // create an "off-screen" anchor tag
+    var lnk = document.createElement('a'),
+        e;
+
+    // the key here is to set the download attribute of the a tag
+    lnk.download = filename;
+
+    // convert canvas content to data-uri for link. When download
+    // attribute is set the content pointed to by link will be
+    // pushed as "download" in HTML5 capable browsers
+    lnk.href = canvas.toDataURL();
+
+    // create a "fake" click-event to trigger the download
+    if (document.createEvent) {
+
+        e = document.createEvent("MouseEvents");
+        e.initMouseEvent("click", true, true, window,
+                         0, 0, 0, 0, 0, false, false, false,
+                         false, 0, null);
+
+        lnk.dispatchEvent(e);
+
+    } else if (lnk.fireEvent) {
+
+        lnk.fireEvent("onclick");
+    }
+};
+//********
+
 // Keep track of the mouse button being pressed and draw a dot at current location
 function sketch_mouseDown() {
     mouseDown=1;
